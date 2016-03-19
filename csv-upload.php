444912b31ff60c53a6e9c0169ac3e29196b382f2
Erik Mitchell <erikdmitchell@gmail.com>
2016-03-19 10:13:40 -0400

    added a templating system file

--- csv-upload.php
+++ csv-upload.php
@@ -5,4 +5,10 @@ define('EMCSVUPLOAD_PATH', plugin_dir_path(__FILE__));
 // require our files //
 require_once(EMCSVUPLOAD_PATH.'class-csv-upload.php');
 require_once(EMCSVUPLOAD_PATH.'functions.php');
+
+/*
+hook our admin pages up
+the idea is to have a function/shortcode we can drop in that will basically run itself
+it would have our admin pages and then it could run within the admin section on its own
+*/
 ?>
