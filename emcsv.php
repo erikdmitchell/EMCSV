1b8c4d1ec84fd556bb86413be35006abdc588e7f
Erik Mitchell <erikdmitchell@gmail.com>
2016-04-09 08:48:20 -0400

    renamved emcsv and added github updater

--- emcsv.php
+++ emcsv.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: EMCSV
 Plugin URI:
-Description: Coming soon.
+Description: A powerful yet simple CSV file uploader.
 Version:     0.1.1
 Author:      Erik Mitchell
 Author URI:  http://erikmitchell.net
@@ -13,12 +13,13 @@ Text Domain: emcsv
 */
 
 // set/define some globals //
-define('EMCSVUPLOAD_PATH', plugin_dir_path(__FILE__));
+define('EMCSV_PATH', plugin_dir_path(__FILE__));
 
 // require our files //
-require_once(EMCSVUPLOAD_PATH.'functions.php'); // general functions for the plugin
-require_once(EMCSVUPLOAD_PATH.'custom-maps.php'); // custom maps functionality
-require_once(EMCSVUPLOAD_PATH.'lib/get-template-part.php'); // a get template part function
+require_once(EMCSV_PATH.'functions.php'); // general functions for the plugin
+require_once(EMCSV_PATH.'custom-maps.php'); // custom maps functionality
+require_once(EMCSV_PATH.'lib/get-template-part.php'); // a get template part function
+require_once(EMCSV_PATH.'lib/updater/updater.php'); // our github updater class
 
 $emcsv_active=true;
 
@@ -128,4 +129,32 @@ function emcsv_admin_scripts_styles($hook) {
 	wp_enqueue_style('emcsv-admin-style', plugins_url('css/admin.css', __FILE__)); // all
 }
 add_action('admin_enqueue_scripts','emcsv_admin_scripts_styles');
+
+/**
+ * emcsv_github_update_check function.
+ *
+ * ur github update settings
+ *
+ * @access public
+ * @return void
+ */
+function emcsv_github_update_check() {
+	if (is_admin()) : // note the use of is_admin() to double check that this is happening in the admin
+	    $config = array(
+	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+	        'proper_folder_name' => 'emcsv', // this is the name of the folder your plugin lives in
+	        'api_url' => 'https://api.github.com/repos/username/repository-name', // the github API url of your github repo
+	        'raw_url' => 'https://raw.github.com/username/repository-name/master', // the github raw url of your github repo
+	        'github_url' => 'https://github.com/username/repository-name', // the github url of your github repo
+	        'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the github repo
+	        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+	        'requires' => '3.0', // which version of WordPress does your plugin require?
+	        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+	        'readme' => 'README.md', // which file to use as the readme for the version number
+	        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
+	    );
+	    new WP_GitHub_Updater($config);
+	endif;
+}
+add_action('admin_init', 'emcsv_github_update_check');
 ?>
---
+++ lib/updater/index.php
@@ -0,0 +1,1 @@
+<?php // ye shall not pass
